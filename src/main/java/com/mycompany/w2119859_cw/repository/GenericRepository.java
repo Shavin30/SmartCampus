976c300339bc30f1c28c33dafee7f417c4d1a281
Shavin30 <shavindesilva360@gmail.com>
2026-04-16 18:03:16 +0530

    Add update method

--- src/main/java/com/mycompany/w2119859_cw/repository/GenericRepository.java
+++ src/main/java/com/mycompany/w2119859_cw/repository/GenericRepository.java
@@ -4,6 +4,7 @@ import com.mycompany.w2119859_cw.model.BaseModel;
 import java.util.List;
 
 public class GenericRepository<T extends BaseModel> {
+
     private final List<T> items;
 
     public GenericRepository(List<T> items) {
@@ -16,13 +17,22 @@ public class GenericRepository<T extends BaseModel> {
 
     public T getById(String id) {
         return items.stream()
-                    .filter(item -> item.getId().equals(id))
-                    .findFirst()
-                    .orElse(null);
+                .filter(item -> item.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 
     public void add(T item) {
         items.add(item);
+    }
+
+    public void update(T updatedItem) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getId().equals(updatedItem.getId())) {
+                items.set(i, updatedItem); // Replaces the old object in memory
+                return;
+            }
+        }
     }
 
     public void delete(String id) {
