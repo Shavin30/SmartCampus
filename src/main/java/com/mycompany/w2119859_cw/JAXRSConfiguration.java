3dff466ebe06397e008f126a7697393f7199b57b
Shavin30 <shavindesilva360@gmail.com>
2026-04-16 14:55:33 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/w2119859_cw/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.w2119859_cw;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
